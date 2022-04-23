61c29cfcc584f78fe4482752973b654d215b78c2
M.Idrees <muhammadidrees7260@gmail.com>
2022-04-24 03:59:10 +0500

    Initial commit

---
+++ app/src/main/java/com/example/sp20__bse_023_assi_2/ShowMessage.java
@@ -0,0 +1,14 @@
+package com.example.sp20__bse_023_assi_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_message);
+    }
+}
